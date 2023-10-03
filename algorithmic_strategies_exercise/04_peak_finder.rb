4ba56eca1be94f20b71635132d7554b8a9783b63
abaez8031 <abaez8031@gmail.com>
2023-10-02 20:33:48 -0400

    peakfinder

--- algorithmic_strategies_exercise/04_peak_finder.rb
+++ algorithmic_strategies_exercise/04_peak_finder.rb
@@ -4,7 +4,20 @@
 # The first or last element of the array is considered a "peak" if it is greater than it's one neighbor.
 
 def peak_finder(arr)
-  
+  newArr = []
+  (0...arr.length).each do |i|
+    curr = arr[i]
+    nex = arr[i + 1]
+    prev = arr[i - 1]
+    if i == 0 && curr > nex
+      newArr.push(curr)
+    elsif i != 0 && i != arr.length - 1 && curr > nex && curr > prev
+      newArr.push(curr)
+    elsif i == arr.length - 1 && curr > prev
+      newArr.push(curr)
+    end
+  end
+  newArr
 end
 
 p peak_finder([1, 3, 5, 4])         # => [5]
