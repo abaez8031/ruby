c8a970b081f8e5016abeede2ccf5a31b6dd603ea
abaez8031 <abaez8031@gmail.com>
2023-10-04 14:41:34 -0400

    perilousphase1

--- Day4/perilous_procs_phase1.rb
+++ Day4/perilous_procs_phase1.rb
@@ -1,7 +1,11 @@
 # some?
 # Write a method some? that accepts an array and a block as arguments. The method should return a boolean indicating whether or not at least one of the elements of the array returns true when given to the block. Solve this using Array#each.
 
-def some?
+def some?(arr, &prc)
+  arr.each do |ele|
+    return true if prc.call(ele)
+  end
+  false
 end
 
 # p some?([3, 1, 11, 5]) { |n| n.even? }                                # false
@@ -14,7 +18,12 @@ end
 # exactly?
 # Write a method exactly? that accepts an array, a number (n), and a block as arguments. The method should return a boolean indicating whether or not there are exactly n elements that return true when given to the block. Solve this using Array#each.
 
-def exactly?
+def exactly?(arr,n, &prc)
+  count = 0
+  arr.each do |ele|
+    count += 1 if prc.call(ele)
+  end
+  count == n
 end
 
 # p exactly?(['A', 'b', 'C'], 2) { |el| el == el.upcase }         # true
@@ -26,20 +35,32 @@ end
 # p exactly?([4, 5], 3) { |n| n > 0 }                             # false
 # p exactly?([4, 5, 2], 3) { |n| n > 0 }                          # true
 # p exactly?([42, -9, 7, -3, -6], 2) { |n| n > 0 }                # true
+
 # filter_out
 # Write a method filter_out that accepts an array and a block as arguments. The method should return a new array where elements of the original array are removed if they return true when given to the block. Solve this using Array#each.
 
-def filter_out
+def filter_out(arr, &prc)
+  filtered = []
+  arr.each do |ele|
+    filtered << ele if !prc.call(ele)
+  end
+  filtered
 end
 
 # p filter_out([10, 6, 3, 2, 5 ]) { |x| x.odd? }      # [10, 6, 2]
 # p filter_out([1, 7, 3, 5 ]) { |x| x.odd? }          # []
 # p filter_out([10, 6, 3, 2, 5 ]) { |x| x.even? }     # [3, 5]
 # p filter_out([1, 7, 3, 5 ]) { |x| x.even? }         # [1, 7, 3, 5]
+
 # at_least?
 # Write a method at_least? that accepts an array, a number (n), and a block as an arguments. The method should return a boolean indicating whether or not at least n elements of the array return true when given to the block. Solve this using Array#each.
 
-def at_least?
+def at_least?(arr,n,&prc)
+  count = 0
+  arr.each do |ele|
+    count += 1 if prc.call(ele)
+  end
+  count >= n
 end
 
 # p at_least?(['sad', 'quick', 'timid', 'final'], 2) { |s| s.end_with?('ly') }
@@ -66,7 +87,11 @@ end
 # every?
 # Write a method every? that accepts an array and a block as arguments. The method should return a boolean indicating whether or not all elements of the array return true when given to the block. Solve this using Array#each.
 
-def every?
+def every?(arr, &prc)
+  arr.each do |ele|
+    return false if !prc.call(ele)
+  end
+  true
 end
 
 # p every?([3, 1, 11, 5]) { |n| n.even? }                                 # false
@@ -75,10 +100,16 @@ end
 # p every?(['squash', 'corn', 'kale', 'carrot']) { |str| str[0] == 'p' }  # false
 # p every?(['squash', 'pea', 'kale', 'potato']) { |str| str[0] == 'p' }   # false
 # p every?(['parsnip', 'potato', 'pea']) { |str| str[0] == 'p' }          # true
+
 # at_most?
 # Write a method at_most? that accepts an array, a number (n), and a block as arguments. The method should return a boolean indicating whether no more than n elements of the array return true when given to the block. Solve this using Array#each.
 
-def at_most?
+def at_most?(arr,n, &prc)
+  count = 0
+  arr.each do |ele|
+    count += 1 if prc.call(ele)
+  end
+  count <= n
 end
 
 # p at_most?([-4, 100, -3], 1) { |el| el > 0 }                         # true
@@ -92,12 +123,16 @@ end
 # first_index
 # Write a method first_index that accepts an array and a block as arguments. The method should return the index of the first element of the array that returns true when giben to the block. If no element of returns true, then the method should return nil. Solve this using Array#each.
 
-def first_index
+def first_index(arr, &prc)
+  arr.each do |ele|
+    return arr.index(ele) if prc.call(ele)
+  end
+  nil
 end
 
-# p first_index(['bit', 'cat', 'byte', 'below']) { |el| el.length > 3 }           # 2
-# p first_index(['bitten', 'bit', 'cat', 'byte', 'below']) { |el| el.length > 3 } # 0
-# p first_index(['bitten', 'bit', 'cat', 'byte', 'below']) { |el| el.length > 6 } # nil
-# p first_index(['bit', 'cat', 'byte', 'below']) { |el| el[0] == 'b' }            # 0
-# p first_index(['bit', 'cat', 'byte', 'below']) { |el| el.include?('a') }        # 1
-# p first_index(['bit', 'cat', 'byte', 'below']) { |el| el[0] == 't' }            # nil
+p first_index(['bit', 'cat', 'byte', 'below']) { |el| el.length > 3 }           # 2
+p first_index(['bitten', 'bit', 'cat', 'byte', 'below']) { |el| el.length > 3 } # 0
+p first_index(['bitten', 'bit', 'cat', 'byte', 'below']) { |el| el.length > 6 } # nil
+p first_index(['bit', 'cat', 'byte', 'below']) { |el| el[0] == 'b' }            # 0
+p first_index(['bit', 'cat', 'byte', 'below']) { |el| el.include?('a') }        # 1
+p first_index(['bit', 'cat', 'byte', 'below']) { |el| el[0] == 't' }            # nil
