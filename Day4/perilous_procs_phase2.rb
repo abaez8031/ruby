dd61708606d87aca78856efaf877f79a992ac679
abaez8031 <abaez8031@gmail.com>
2023-10-04 14:57:10 -0400

    perilousphase2

--- Day4/perilous_procs_phase2.rb
+++ Day4/perilous_procs_phase2.rb
@@ -1,6 +1,15 @@
 # xnor_select
 # Write a method xnor_select that accepts an array and two procs as arguments. The method should return a new array containing elements of the original array that either return true for both procs or return false for both procs.
 
+def xnor_select(arr,prc1,prc2)
+  new_arr = []
+  arr.each do |ele|
+    new_arr << ele if prc1.call(ele) && prc2.call(ele)
+    new_arr << ele if !prc1.call(ele) && !prc2.call(ele)
+  end
+  new_arr
+end
+
 # Examples
 
 # is_even = Proc.new { |n| n % 2 == 0 }
@@ -9,8 +18,23 @@
 # p xnor_select([8, 3, -4, -5], is_even, is_positive)         # [8, -5]
 # p xnor_select([-7, -13, 12, 5, -10], is_even, is_positive)  # [-7, -13, 12]
 # p xnor_select([-7, -13, 12, 5, -10], is_odd, is_positive)   # [5, -10]
+
+
 # filter_out!
 # Rewrite your previous filter_out method to mutate the input array instead of returning a new array. That is, write a method filter_out! that accepts an array and a block as args. The method should remove elements of the input array that return true when given to the block. Solve this without using Array.reject!.
+
+def filter_out!(arr, &prc)
+  i = 0
+  while i < arr.length
+    ele = arr[i]
+    if prc.call(ele)
+      arr.delete(ele)
+    else
+      i += 1
+    end
+  end
+  arr
+end
 
 # Examples
 
@@ -29,8 +53,17 @@
 # arr_4 = [1, 7, 3, 5 ]
 # filter_out!([1, 7, 3, 5 ]) { |x| x.even? }
 # p arr_4 # [1, 7, 3, 5]
+
 # multi_map
 # Write a method multi_map that accepts an array, an optional number (n), and a block as arguments. The method should return a new array where each element of the original array is repeatedly run through the block n times. If the number argument is not passed in, then the the elements should be run through the block once.
+
+def multi_map(arr, n = 1, &prc)
+  new_arr = arr.map do |ele|
+    n.times { ele = prc.call(ele) }
+    ele
+  end
+  new_arr
+end
 
 # Examples
 
@@ -40,8 +73,15 @@
 # p multi_map([4, 3, 2, 7], 1) { |num| num * 10 }             # [40, 30, 20, 70]
 # p multi_map([4, 3, 2, 7], 2) { |num| num * 10 }             # [400, 300, 200, 700]
 # p multi_map([4, 3, 2, 7], 4) { |num| num * 10 }             # [40000, 30000, 20000, 70000]
+
 # proctition
 # Write a method proctition that accepts an array and a block as arguments. The method should return a new array where the elements that return true when given to the block come before all of the elements that return false when given to the block. This means that the new array has two partitions; the order among elements in the same partition should be the same as their relative order in the input array.
+
+def proctition(arr, &prc)
+  trues = arr.select { |ele| prc.call(ele) }
+  falses = arr.select { |ele| !prc.call(ele) }
+  [*trues, *falses]
+end
 
 # Examples
 
