058c63a3782d7d281c28f0868b0e61879900644e
abaez8031 <abaez8031@gmail.com>
2023-10-04 14:57:44 -0400

    perilousprocs

--- Day4/perilous_procs_phase1.rb
+++ Day4/perilous_procs_phase1.rb
@@ -130,9 +130,9 @@ def first_index(arr, &prc)
   nil
 end
 
-p first_index(['bit', 'cat', 'byte', 'below']) { |el| el.length > 3 }           # 2
-p first_index(['bitten', 'bit', 'cat', 'byte', 'below']) { |el| el.length > 3 } # 0
-p first_index(['bitten', 'bit', 'cat', 'byte', 'below']) { |el| el.length > 6 } # nil
-p first_index(['bit', 'cat', 'byte', 'below']) { |el| el[0] == 'b' }            # 0
-p first_index(['bit', 'cat', 'byte', 'below']) { |el| el.include?('a') }        # 1
-p first_index(['bit', 'cat', 'byte', 'below']) { |el| el[0] == 't' }            # nil
+# p first_index(['bit', 'cat', 'byte', 'below']) { |el| el.length > 3 }           # 2
+# p first_index(['bitten', 'bit', 'cat', 'byte', 'below']) { |el| el.length > 3 } # 0
+# p first_index(['bitten', 'bit', 'cat', 'byte', 'below']) { |el| el.length > 6 } # nil
+# p first_index(['bit', 'cat', 'byte', 'below']) { |el| el[0] == 'b' }            # 0
+# p first_index(['bit', 'cat', 'byte', 'below']) { |el| el.include?('a') }        # 1
+# p first_index(['bit', 'cat', 'byte', 'below']) { |el| el[0] == 't' }            # nil
