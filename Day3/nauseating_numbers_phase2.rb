8fa670ac92b96873948f7fa5ee82b4930ed3caa0
abaez8031 <abaez8031@gmail.com>
2023-10-04 01:11:41 -0400

    mutualfactors

--- Day3/nauseating_numbers_phase2.rb
+++ Day3/nauseating_numbers_phase2.rb
@@ -49,21 +49,26 @@ def matrix_addition(matrix1,matrix2)
   return_matrix
 end
 
-matrix_a = [[2,5], [4,7]]
-matrix_b = [[9,1], [3,0]]
-matrix_c = [[-1,0], [0,-1]]
-matrix_d = [[2, -5], [7, 10], [0, 1]]
-matrix_e = [[0 , 0], [12, 4], [6,  3]]
+# matrix_a = [[2,5], [4,7]]
+# matrix_b = [[9,1], [3,0]]
+# matrix_c = [[-1,0], [0,-1]]
+# matrix_d = [[2, -5], [7, 10], [0, 1]]
+# matrix_e = [[0 , 0], [12, 4], [6,  3]]
 
-p matrix_addition(matrix_a, matrix_b) # [[11, 6], [7, 7]]
-p matrix_addition(matrix_a, matrix_c) # [[1, 5], [4, 6]]
-p matrix_addition(matrix_b, matrix_c) # [[8, 1], [3, -1]]
-p matrix_addition(matrix_d, matrix_e) # [[2, -5], [19, 14], [6, 4]]
+# p matrix_addition(matrix_a, matrix_b) # [[11, 6], [7, 7]]
+# p matrix_addition(matrix_a, matrix_c) # [[1, 5], [4, 6]]
+# p matrix_addition(matrix_b, matrix_c) # [[8, 1], [3, -1]]
+# p matrix_addition(matrix_d, matrix_e) # [[2, -5], [19, 14], [6, 4]]
 
 # mutual_factors
 # Write a method mutual_factors that accepts any amount of numbers as arguments. The method should return an array containing all of the common divisors shared among the arguments. For example, the common divisors of 50 and 30 are [1, 2, 5, 10]. You can assume that all of the arguments are positive integers.
 
 def mutual_factors(*args)
+  mutual_factors = []
+  (1..args[0]).each do |num|
+    mutual_factors << num if args.all? { |arg| arg % num == 0}
+  end
+  mutual_factors
 end
 
 # p mutual_factors(50, 30)            # [1, 2, 5, 10]
