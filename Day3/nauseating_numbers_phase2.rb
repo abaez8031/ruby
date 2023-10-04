33c75396e0076bc502ce4a9d5c72ffec07d321a7
abaez8031 <abaez8031@gmail.com>
2023-10-04 01:02:46 -0400

    matrix_addition

--- Day3/nauseating_numbers_phase2.rb
+++ Day3/nauseating_numbers_phase2.rb
@@ -18,16 +18,16 @@ def anti_prime?(num)
 end
 
 
-p anti_prime?(24)   # true
-p anti_prime?(36)   # true
-p anti_prime?(48)   # true
-p anti_prime?(360)  # true
-p anti_prime?(1260) # true
-p anti_prime?(27)   # false
-p anti_prime?(5)    # false
-p anti_prime?(100)  # false
-p anti_prime?(136)  # false
-p anti_prime?(1024) # false
+# p anti_prime?(24)   # true
+# p anti_prime?(36)   # true
+# p anti_prime?(48)   # true
+# p anti_prime?(360)  # true
+# p anti_prime?(1260) # true
+# p anti_prime?(27)   # false
+# p anti_prime?(5)    # false
+# p anti_prime?(100)  # false
+# p anti_prime?(136)  # false
+# p anti_prime?(1024) # false
 
 # matrix_addition
 # Let a 2-dimensional array be known as a "matrix". Write a method matrix_addition that accepts two matrices as arguments. The two matrices are guaranteed to have the same "height" and "width". The method should return a new matrix representing the sum of the two arguments. To add matrices, we simply add the values at the same positions:
@@ -40,18 +40,25 @@ p anti_prime?(1024) # false
 # 4 7     3 0      7 7
 
 def matrix_addition(matrix1,matrix2)
+  return_matrix = Array.new(matrix1.length)  {Array.new(matrix1[0].length)}
+  (0...matrix1.length).each do |i|
+    (0...matrix1[0].length).each do |j|
+      return_matrix[i][j] = matrix1[i][j] + matrix2[i][j]
+    end
+  end
+  return_matrix
 end
 
-# matrix_a = [[2,5], [4,7]]
-# matrix_b = [[9,1], [3,0]]
-# matrix_c = [[-1,0], [0,-1]]
-# matrix_d = [[2, -5], [7, 10], [0, 1]]
-# matrix_e = [[0 , 0], [12, 4], [6,  3]]
+matrix_a = [[2,5], [4,7]]
+matrix_b = [[9,1], [3,0]]
+matrix_c = [[-1,0], [0,-1]]
+matrix_d = [[2, -5], [7, 10], [0, 1]]
+matrix_e = [[0 , 0], [12, 4], [6,  3]]
 
-# p matrix_addition(matrix_a, matrix_b) # [[11, 6], [7, 7]]
-# p matrix_addition(matrix_a, matrix_c) # [[1, 5], [4, 6]]
-# p matrix_addition(matrix_b, matrix_c) # [[8, 1], [3, -1]]
-# p matrix_addition(matrix_d, matrix_e) # [[2, -5], [19, 14], [6, 4]]
+p matrix_addition(matrix_a, matrix_b) # [[11, 6], [7, 7]]
+p matrix_addition(matrix_a, matrix_c) # [[1, 5], [4, 6]]
+p matrix_addition(matrix_b, matrix_c) # [[8, 1], [3, -1]]
+p matrix_addition(matrix_d, matrix_e) # [[2, -5], [19, 14], [6, 4]]
 
 # mutual_factors
 # Write a method mutual_factors that accepts any amount of numbers as arguments. The method should return an array containing all of the common divisors shared among the arguments. For example, the common divisors of 50 and 30 are [1, 2, 5, 10]. You can assume that all of the arguments are positive integers.
