0104d955a3157a747df1f68bab1e3f8abddd88d9
abaez8031 <abaez8031@gmail.com>
2023-10-05 18:35:15 -0400

    meddling

--- Week_1/Day5/meddling_medley_phase2.rb
+++ Week_1/Day5/meddling_medley_phase2.rb
@@ -1,6 +1,12 @@
 # conjunct_select
 # Write a method conjunct_select that accepts an array and one or more procs as arguments. The method should return a new array containing the elements that return true when passed into all of the given procs.
 
+def conjunct_select(arr, *prcs)
+  arr.select do |ele| 
+    prcs.all? { |prc| prc.call(ele) }
+  end
+end
+
 # Examples
 
 # is_positive = Proc.new { |n| n > 0 }
@@ -10,6 +16,7 @@
 # p conjunct_select([4, 8, -2, 11, 7, -3, 13], is_positive) # [4, 8, 11, 7, 13]
 # p conjunct_select([4, 8, -2, 11, 7, -3, 13], is_positive, is_odd) # [11, 7, 13]
 # p conjunct_select([4, 8, -2, 11, 7, -3, 13], is_positive, is_odd, less_than_ten) # [7]
+
 # convert_pig_latin
 # Write a method convert_pig_latin that accepts a sentence as an argument. The method should translate the sentence according to the following rules:
 
@@ -19,6 +26,9 @@
 # if the word begins with a non-vowel, move all letters that come before the word's first vowel to the end of the word and add 'ay' (example: 'trash'->'ashtray')
 # Note that if words are capitalized in the original sentence, they should remain capitalized in the translated sentence. Vowels are the letters a, e, i, o, u.
 
+def convert_pig_latin(sentence)
+end
+
 # Examples
 
 # p convert_pig_latin('We like to eat bananas') # "We ikelay to eatyay ananasbay"
@@ -26,6 +36,7 @@
 # p convert_pig_latin('What an interesting problem') # "Atwhay an interestingyay oblempray"
 # p convert_pig_latin('Her family flew to France') # "Erhay amilyfay ewflay to Ancefray"
 # p convert_pig_latin('Our family flew to France') # "Ouryay amilyfay ewflay to Ancefray"
+
 # reverberate
 # Write a method reverberate that accepts a sentence as an argument. The method should translate the sentence according to the following rules:
 
@@ -35,16 +46,23 @@
 # if the word ends with a non-vowel, repeat all letters that come after the word's last vowel, including the last vowel itself (example: 'trash'->'trashash')
 # Note that if words are capitalized in the original sentence, they should remain capitalized in the translated sentence. Vowels are the letters a, e, i, o, u.
 
+def reverberate(sentence)
+end
+
 # Examples
 
 # p reverberate('We like to go running fast') # "We likelike to go runninging fastast"
 # p reverberate('He cannot find the trash') # "He cannotot findind thethe trashash"
 # p reverberate('Pasta is my favorite dish') # "Pastapasta is my favoritefavorite dishish"
 # p reverberate('Her family flew to France') # "Herer familyily flewew to Francefrance"
+
 # disjunct_select
 # Write a method disjunct_select that accepts an array and one or more procs as arguments. The method should return a new array containing the elements that return true when passed into at least one of the given procs.
 
 # Examples
+
+def disjunct_select(arr, *prcs)
+end
 
 # longer_four = Proc.new { |s| s.length > 4 }
 # contains_o = Proc.new { |s| s.include?('o') }
@@ -64,6 +82,7 @@
 #     contains_o,
 #     starts_a
 # ) # ["ace", "dog", "apple", "teeming", "boot"]
+
 # alternating_vowel
 # Write a method alternating_vowel that accepts a sentence as an argument. The method should return a new sentence where the words alternate between having their first or last vowel removed. For example:
 
@@ -76,10 +95,14 @@
 
 # Examples
 
+def alternating_vowel(sentence)
+end
+
 # p alternating_vowel('panthers are great animals') # "pnthers ar grat animls"
 # p alternating_vowel('running panthers are epic') # "rnning panthrs re epc"
 # p alternating_vowel('code properly please') # "cde proprly plase"
 # p alternating_vowel('my forecast predicts rain today') # "my forecst prdicts ran tday"
+
 # silly_talk
 # Write a method silly_talk that accepts a sentence as an argument. The method should translate each word of the sentence according to the following rules:
 
@@ -89,14 +112,21 @@
 
 # Examples
 
+def silly_talk(sentence)
+end
+
 # p silly_talk('Kids like cats and dogs') # "Kibids likee cabats aband dobogs"
 # p silly_talk('Stop that scooter') # "Stobop thabat scobooboteber"
 # p silly_talk('They can code') # "Thebey caban codee"
 # p silly_talk('He flew to Italy') # "Hee flebew too Ibitabaly"
+
 # compress
 # Write a method compress that accepts a string as an argument. The method should return a "compressed" version of the string where streaks of consecutive letters are translated to a single appearance of the letter followed by the number of times it appears in the streak. If a letter does not form a streak (meaning that it appears alone), then do not add a number after it.
 
 # Examples
+
+def compress(str)
+end
 
 # p compress('aabbbbc')   # "a2b4c"
 # p compress('boot')      # "bo2t"
