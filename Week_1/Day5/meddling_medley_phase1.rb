5822ea51ea267b85a610fc5860eaa8718d15b08b
abaez8031 <abaez8031@gmail.com>
2023-10-05 18:03:23 -0400

    meddling_phase1

--- Week_1/Day5/meddling_medley_phase1.rb
+++ Week_1/Day5/meddling_medley_phase1.rb
@@ -13,14 +13,26 @@ def duos(str)
   count
 end
 
-p duos('bootcamp')      # 1
-p duos('wxxyzz')        # 2
-p duos('hoooraay')      # 3
-p duos('dinosaurs')     # 0
-p duos('e')             # 0
+# p duos('bootcamp')      # 1
+# p duos('wxxyzz')        # 2
+# p duos('hoooraay')      # 3
+# p duos('dinosaurs')     # 0
+# p duos('e')             # 0
 
 # sentence_swap
 # Write a method sentence_swap that accepts a sentence and a hash as arguments. The method should return a new sentence where every word is replaced with it's corresponding value in the hash. If a word does not exist as a key of the hash, then it should remain unchanged.
+
+def sentence_swap(sentence,hash)
+  words = sentence.split(" ")
+  new_words = words.map do |word|
+    if hash[word]
+      hash[word]
+    else
+      word
+    end
+  end
+  new_words.join(" ")
+end
 
 # Examples
 
@@ -35,8 +47,17 @@ p duos('e')             # 0
 # p sentence_swap('keep coding okay',
 #     'coding'=>'running', 'kay'=>'pen'
 # ) # 'keep running okay'
+
 # hash_mapped
 # Write a method hash_mapped that accepts a hash, a proc, and a block. The method should return a new hash where each key is the result of the original key when given to the block. Each value of the new hash should be the result of the original values when passed into the proc.
+
+def hash_mapped(hash, prc1, &prc2)
+  new_hash = {}
+  hash.each do |k,v|
+    new_hash[prc2.call(k)] = prc1.call(v)
+  end
+  new_hash
+end
 
 # Examples
 
@@ -47,8 +68,17 @@ p duos('e')             # 0
 # first = Proc.new { |a| a[0] }
 # p hash_mapped({-5=>['q', 'r', 's'], 6=>['w', 'x']}, first) { |n| n * n }
 # # {25=>"q", 36=>"w"}
+
 # counted_characters
 # Write a method counted_characters that accepts a string as an argument. The method should return an array containing the characters of the string that appeared more than twice. The characters in the output array should appear in the same order they occur in the input string.
+
+def counted_characters(str)
+  count = Hash.new(0)
+  str.each_char do |char|
+    count[char] += 1
+  end
+  count.keys.select { |k| count[k] > 2}
+end
 
 # Examples
 
@@ -56,18 +86,44 @@ p duos('e')             # 0
 # p counted_characters("mississippi") # ["i", "s"]
 # p counted_characters("hot potato soup please") # ["o", "t", " ", "p"]
 # p counted_characters("runtime") # []
+
 # triplet_true?
 # Write a method triplet_true? that accepts a string as an argument and returns a boolean indicating whether or not the string contains three of the same character consecutively.
 
+def triplet_true?(str)
+  (0...str.length - 2).each do |i|
+    curr = str[i]
+    nex = str[i + 1]
+    last = str[i + 2]
+    return true if curr == nex && nex == last
+  end
+  false
+end
+
 # Examples
 
-# p triplet_true('caaabb')        # true
-# p triplet_true('terrrrrible')   # true
-# p triplet_true('runninggg')     # true
-# p triplet_true('bootcamp')      # false
-# p triplet_true('e')             # false
+# p triplet_true?('caaabb')        # true
+# p triplet_true?('terrrrrible')   # true
+# p triplet_true?('runninggg')     # true
+# p triplet_true?('bootcamp')      # false
+# p triplet_true?('e')             # false
+
 # energetic_encoding
 # Write a method energetic_encoding that accepts a string and a hash as arguments. The method should return a new string where characters of the original string are replaced with the corresponding values in the hash. If a character is not a key of the hash, then it should be replaced with a question mark ('?'). Space characters (' ') should remain unchanged.
+
+def energetic_encoding(str,hash)
+  chars = str.split("")
+  new_chars = chars.map do |char|
+    if hash[char]
+      hash[char]
+    elsif char != " "
+      "?"
+    else
+      char
+    end
+  end
+  new_chars.join("")
+end
 
 # Examples
 
@@ -84,11 +140,22 @@ p duos('e')             # 0
 # ) # '?arri ?i?r?'
 
 # p energetic_encoding('bike', {}) # '????'
+
 # uncompress
 # Write a method uncompress that accepts a string as an argument. The string will be formatted so every letter is followed by a number. The method should return an "uncompressed" version of the string where every letter is repeated multiple times given based on the number that appears directly after the letter.
 
+def uncompress(str)
+  i = 0
+  output = ""
+  while i < str.length
+    str[i + 1].to_i.times { output += str[i] }
+    i += 2
+  end
+  output
+end
+
 # Examples
 
-# uncompress('a2b4c1') # 'aabbbbc'
-# uncompress('b1o2t1') # 'boot'
-# uncompress('x3y1x2z4') # 'xxxyxxzzzz'
+# p uncompress('a2b4c1') # 'aabbbbc'
+# p uncompress('b1o2t1') # 'boot'
+# p uncompress('x3y1x2z4') # 'xxxyxxzzzz'
