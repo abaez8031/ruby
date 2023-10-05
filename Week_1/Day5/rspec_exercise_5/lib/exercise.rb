c715daa9f2fbcc16dfa52fcda7fa9731383fc590
abaez8031 <abaez8031@gmail.com>
2023-10-05 00:26:27 -0400

    day6

---
+++ Week_1/Day5/rspec_exercise_5/lib/exercise.rb
@@ -0,0 +1,20 @@
+def zip(*arrays)
+end
+
+def prizz_proc(arr, prc1,prc2)
+end
+
+def zany_zip(*arrays)
+end
+
+def maximum(arr, &prc)
+end
+
+def my_group_by(arr, &prc)
+end
+
+def max_tie_breaker(arr, prc1, &prc2)
+end
+
+def silly_syllables(sentence)
+end
