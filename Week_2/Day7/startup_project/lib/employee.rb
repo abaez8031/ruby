2dbad8b56ae272a6fef7cc42db8453622dcebf42
abaez8031 <abaez8031@gmail.com>
2023-10-07 01:35:35 -0400

    employee

--- Week_2/Day7/startup_project/lib/employee.rb
+++ Week_2/Day7/startup_project/lib/employee.rb
@@ -1,3 +1,13 @@
 class Employee
+  attr_reader :name, :title
+  def initialize(name,title)
+    @title = title
+    @name = name
+    @earnings = 0
+  end
+
+  def pay(amt)
+    @earnings += amt
+  end
 
 end
