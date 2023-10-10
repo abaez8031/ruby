cbc3eecaf4a2e134b1022abef5a2ffbbb9e0681f
abaez8031 <abaez8031@gmail.com>
2023-10-10 19:00:12 -0400

    memory_puzzle

---
+++ Week_3/Day12/Memory_Puzzle/card.rb
@@ -0,0 +1,29 @@
+class Card
+  def initialize(val)
+    @val = val
+    @face_up = false
+  end
+
+  def hide
+    @face_up = false
+  end
+
+  def reveal
+    @face_up = true
+  end
+
+  def display
+    @val if @face_up
+  end
+
+  def to_s
+    
+  end
+  
+  def ==(card)
+    return true if @val == card.val
+    false
+  end
+
+
+end
