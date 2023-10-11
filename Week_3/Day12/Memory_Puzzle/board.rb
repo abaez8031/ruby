66cfb3c8bc7d70587da4922613fd63cbe2bca6bc
abaez8031 <abaez8031@gmail.com>
2023-10-11 15:09:29 -0400

    memory

---
+++ Week_3/Day12/Memory_Puzzle/board.rb
@@ -0,0 +1,28 @@
+require_relative "card.rb"
+
+class Board
+
+  def initialize
+    @grid = Array.new(4) { Array.new(4) }
+  end
+
+
+  def populate
+  end
+
+  def render
+  end
+
+  def won?
+  end
+
+  def reveal
+  end
+
+  def [](pos)
+  end
+
+  def []=(pos,val)
+  end
+
+end
