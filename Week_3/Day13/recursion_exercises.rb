ff363c309ba3fbf96617b1af8f5d2f36bc0e8947
abaez8031 <abaez8031@gmail.com>
2023-10-12 17:52:25 -0400

    range

--- Week_3/Day13/recursion_exercises.rb
+++ Week_3/Day13/recursion_exercises.rb
@@ -1,8 +1,15 @@
 # Warmup
 # Write a recursive method, range, that takes a start and an end and returns an array of all numbers in that range, exclusive. For example, range(1, 5) should return [1, 2, 3, 4]. If end < start, you can return an empty array.
 
-def range(start, end)
+def range(start, finish)
+  arr = []
+  i = start
+  while i < finish
+    arr += range(i + 1, finish)
+  end
+  arr
 end
+
 
 # Write both a recursive and iterative version of sum of an array.
 
