b37e006d4822922ccabbc77793c9f13e6908dee4
abaez8031 <abaez8031@gmail.com>
2023-10-16 15:50:52 -0400

    recursion

--- Week_3/Day13/recursion_hw.rb
+++ Week_3/Day13/recursion_hw.rb
@@ -2,13 +2,16 @@
 # Write a function sum_to(n) that uses recursion to calculate the sum from 1 to n (inclusive of n).
 
 def sum_to(n)
+  return nil if n < 0
+  return 0 if n < 1
+  n + sum_to(n - 1)
 end
 
 #   # Test Cases
-#   sum_to(5)  # => returns 15
-#   sum_to(1)  # => returns 1
-#   sum_to(9)  # => returns 45
-#   sum_to(-8)  # => returns nil
+  p sum_to(5)  # => returns 15
+  p sum_to(1)  # => returns 1
+  p sum_to(9)  # => returns 45
+  p sum_to(-8)  # => returns nil
 
 # Exercise 2 - add_numbers
 # Write a function add_numbers(nums_array) that takes in an array of Integers and returns the sum of those numbers. Write this method recursively.
