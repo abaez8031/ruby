0b6bf5dc522c1ee2146c9c1298caffe2f2cf8192
abaez8031 <abaez8031@gmail.com>
2023-10-17 16:32:58 -0400

    recursion

--- Week_3/Day13/recursion_hw.rb
+++ Week_3/Day13/recursion_hw.rb
@@ -41,16 +41,22 @@ end
   p gamma_fnc(1)  # => returns 1
   p gamma_fnc(4)  # => returns 6
   p gamma_fnc(8)  # => returns 5040
-  
+
 # Exercise 4 - Ice Cream Shop
 # Write a function ice_cream_shop(flavors, favorite) that takes in an array of ice cream flavors available at the ice cream shop, as well as the user's favorite ice cream flavor. Recursively find out whether or not the shop offers their favorite flavor.
 
+def ice_cream_shop(flavors, favorite)
+  return false if flavors.empty?
+  return true if flavors.last == favorite
+  ice_cream_shop(flavors[0...-1], favorite)
+end
+
 #   # Test Cases
-#   ice_cream_shop(['vanilla', 'strawberry'], 'blue moon')  # => returns false
-#   ice_cream_shop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea')  # => returns true
-#   ice_cream_shop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea salt caramel'], 'pistachio')  # => returns false
-#   ice_cream_shop(['moose tracks'], 'moose tracks')  # => returns true
-#   ice_cream_shop([], 'honey lavender')  # => returns false
+  # p ice_cream_shop(['vanilla', 'strawberry'], 'blue moon')  # => returns false
+  # p ice_cream_shop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea')  # => returns true
+  # p ice_cream_shop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea salt caramel'], 'pistachio')  # => returns false
+  # p ice_cream_shop(['moose tracks'], 'moose tracks')  # => returns true
+  # p ice_cream_shop([], 'honey lavender')  # => returns false
 # Exercise 5 - Reverse
 # Write a function reverse(string) that takes in a string and returns it reversed.
 
