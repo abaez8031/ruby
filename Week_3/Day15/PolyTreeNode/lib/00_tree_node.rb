67cff4b889a2c375b2dfaad3bde438db2e364119
abaez8031 <abaez8031@gmail.com>
2023-10-30 16:47:32 -0400

    polytree

--- Week_3/Day15/PolyTreeNode/lib/00_tree_node.rb
+++ Week_3/Day15/PolyTreeNode/lib/00_tree_node.rb
@@ -1,3 +1,16 @@
 class PolyTreeNode
 
+  attr_reader :parent, :children, :value
+
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent=(node)
+    @parent = node
+    node.children << self
+  end
+
 end
